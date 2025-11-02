212bc1738a303be52c6f2de37f85f9f5ed6390a4
NowYouSimmi <josiesimm@hotmail.fr>
2025-11-02 12:32:53 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-// src/App.js
+// src/App.jsx
 import React, { useState } from "react";
 import Header from "./components/Header.jsx";
 
 import Home from "./pages/Home.jsx";
 import InventoryHub from "./pages/InventoryHub.jsx";
 import InventoryEquipment from "./pages/InventoryEquipment.jsx";
-// 👇 change THIS line
-import InventoryInUse from "./pages/inventory-in-use.jsx";
+import InventoryInUse from "./pages/InventoryInUse.jsx";
 import ShowSpecs from "./pages/ShowSpecs.jsx";
 import Venues from "./pages/Venues.jsx";
 import Suppliers from "./pages/Suppliers.jsx";
@@ -35,14 +34,24 @@ export default function App() {
       <Header setPage={setPage} page={page} />
 
       {page === "home" && <Home setPage={setPage} />}
-    {page === "inventory-hub" && <InventoryHub setPage={setPage} />}
-{page === "inventory-equipment" && <InventoryEquipment setPage={setPage} />}
-{page === "inventory-inuse" && <InventoryInUse setPage={setPage} />}
+
+      {/* 👇 INVENTORY PAGES */}
+      {page === "inventory-hub" && <InventoryHub setPage={setPage} />}
+      {page === "inventory-equipment" && (
+        <InventoryEquipment setPage={setPage} />
+      )}
+      {page === "inventory-inuse" && <InventoryInUse setPage={setPage} />}
 
       {page === "showList" && <ShowSpecs />}
+
       {page === "venues" && (
-        <Venues openPDF={openPDF} openGallery={openGallery} setPage={setPage} />
+        <Venues
+          openPDF={openPDF}
+          openGallery={openGallery}
+          setPage={setPage}
+        />
       )}
+
       {page === "suppliers" && <Suppliers setPage={setPage} />}
       {page === "rigCalc" && <RiggingCalc />}
       {page === "schedule" && <Schedule />}
